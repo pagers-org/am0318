06e4034f79506eb3c7d1ebab2cd1ec524ff6dddf
areumsheep <areumsheep@gmail.com>
2022-03-05 20:52:28 +0900

    ✍️Design: 타이틀 폰트 수정

--- packages/stories/src/index.jsx
+++ packages/stories/src/index.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './GlobalStyle';
 
 ReactDOM.render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
