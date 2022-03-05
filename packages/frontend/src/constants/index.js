07c818da6aa87a947820d44edc8b3882d055d14d
parksil0 <sirisiri0809@gmail.com>
2022-03-06 04:14:13 +0900

    ✒️Feat: Main 페이지 생성

--- packages/frontend/src/constants/index.js
+++ packages/frontend/src/constants/index.js
@@ -1,6 +1,12 @@
 export const BASE_URL = 'https://am0318.herokuapp.com/api';
 // export const BASE_URL = 'http://localhost:4001/api'; // 로컬 서버
 
+export const SEND_URL = 'https://localhost:3000/send';
+
+export const TIMEOUT = {
+  COMPLETE_COPY_CLIPBOARD: 2000,
+};
+
 export const ERROR_MESSAGE = {
   HTTP_REQUEST_FAILURE: '요청에 실패했습니다.',
 };
--- packages/frontend/src/pages/Main.jsx
+++ packages/frontend/src/pages/Main.jsx
@@ -1,6 +1,115 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Layout from '../components/Layout';
+import MainImage from '../assets/images/main-image.png';
+import styled from 'styled-components';
+import Input from '../components/Input';
+import Button from '@project/stories/src/components/atom/Button';
+import { BUTTON_THEME, SEND_URL, TIMEOUT, USER } from '../constants';
+import Service from '../service';
+import { getLocalStorage } from '../utils';
+import Snackbar from '@mui/material/Snackbar';
+
+const localstorageNickname = getLocalStorage(USER.NICKNAME);
 
 const Main = () => {
-  return <div>Main</div>;
+  const [nickname, setNickname] = useState(localstorageNickname || '');
+  const [linkShareTheme, setLinkShareTheme] = useState(BUTTON_THEME.DISABLED);
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
+  const copyUrlRef = useRef(null);
+
+  const service = Service();
+
+  useEffect(() => {
+    if (isSnackbarOpen) {
+      setTimeout(() => {
+        setIsSnackbarOpen(false);
+      }, TIMEOUT.COMPLETE_COPY_CLIPBOARD);
+    }
+  }, [isSnackbarOpen]);
+
+  useEffect(() => {
+    nickname
+      ? setLinkShareTheme(BUTTON_THEME.ACTION)
+      : setLinkShareTheme(BUTTON_THEME.DISABLED);
+  }, [nickname]);
+
+  const onChangeName = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const onClickLinkShare = async (e) => {
+    if (!getLocalStorage(USER.NICKNAME)) {
+      const data = await service.signup(nickname);
+      copyUrlRef.current.value = `${SEND_URL}/${data.userId}`;
+    }
+    copyUrlRef.current.select();
+    document.execCommand('copy');
+    e.target.focus();
+    setIsSnackbarOpen(true);
+  };
+
+  return (
+    <Layout title='상장에 적힐 당신의 이름을 멋지게 적어주상!'>
+      <Wrapper>
+        <MainImg src={MainImage} alt='메인 이미지' />
+        <Input
+          type='text'
+          placeholder='10자 이하의 멋진 이름'
+          value={nickname}
+          onChange={onChangeName}
+        />
+      </Wrapper>
+      <ButtonWrapper>
+        <Button
+          theme={linkShareTheme}
+          text='링크 공유'
+          disabled={linkShareTheme === BUTTON_THEME.DISABLED}
+          onClick={onClickLinkShare}
+        ></Button>
+        <ClipboardTextarea
+          ref={copyUrlRef}
+          value={`${SEND_URL}/${getLocalStorage(USER.USER_ID)}`}
+          readOnly
+        />
+        <Button theme={BUTTON_THEME.DEFAULT} text='상장 콜렉션 보기' />
+      </ButtonWrapper>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        open={isSnackbarOpen}
+        onClose={() => setIsSnackbarOpen(false)}
+        message='링크가 복사되었습니다!'
+      />
+    </Layout>
+  );
 };
+
 export default Main;
+
+const MainImg = styled.img`
+  margin: 22px 0px 40px 0px;
+`;
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.div`
+  position: fixed;
+  bottom: 70px;
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  justify-content: flex-end;
+  margin-top: 100px;
+`;
+
+const ClipboardTextarea = styled.textarea`
+  position: absolute;
+  width: 0px;
+  height: 0px;
+  bottom: 0;
+  left: 0;
+  opacity: 0;
+`;
