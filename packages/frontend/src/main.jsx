f77caeb0b250c0ddfe53356d096a98fa8ebcf267
sunhwa <2017sunhwa@gmail.com>
2022-03-05 15:48:46 +0900

    ✒️Feat: route fallback 추가 lazy 오류 수정

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import Spinner from '@project/stories/src/components/atom/Spinner';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <React.Suspense fallback={<Spinner />}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.Suspense>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- packages/frontend/src/pages/Main.jsx
+++ packages/frontend/src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Main = () => {
-  return <div></div>;
+  return <div>Main</div>;
 };
