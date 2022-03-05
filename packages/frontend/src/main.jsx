8ee32c3fb299d29657a41c49605d70ac43f79719
areumsheep <areumsheep@gmail.com>
2022-03-05 20:52:28 +0900

    ✍️Design: 타이틀 폰트 수정

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './GlobalStyle';
 import { BrowserRouter } from 'react-router-dom';
 import Spinner from '@project/stories/src/components/atom/Spinner';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <React.Suspense fallback={<Spinner />}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.Suspense>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
