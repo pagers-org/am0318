0241078d8f30e0593a84cb725ba68ef0ae0c6740
areumsheep <areumsheep@gmail.com>
2022-03-06 03:35:00 +0900

    ✒️Feat: 결과 페이지 추가

--- packages/frontend/src/pages/Complete.jsx
+++ packages/frontend/src/pages/Complete.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import Layout from '../components/Layout';
+import Button from '@project/stories/src/components/atom/Button';
+import ResultAward from '../components/ResultAward';
 
 const Complete = () => {
-  return <div>Complete</div>;
+  const name = '테스트';
+  const sender = '반가워!';
+  return (
+    <Layout title={`${name}가 ${sender}에게 수여할 상장이 준비됐상!`}>
+      <Wrapper>
+        <ResultAward />
+        <ButtonWrapper>
+          <Button theme="action" text="상장 수여하겠상!" />
+          <Button text="나도 받고 싶상!" />
+        </ButtonWrapper>
+      </Wrapper>
+    </Layout>
+  );
 };
 export default Complete;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0px 36px;
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
