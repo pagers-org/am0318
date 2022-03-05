579ddc7d94a342ffc5e1cf241d47ad7a01d36c43
areumsheep <areumsheep@gmail.com>
2022-03-05 17:46:17 +0900

    ✒️Feat: 타이틀 디자인 추가

---
+++ packages/stories/src/components/atom/Title/Title.stories.jsx
@@ -0,0 +1,9 @@
+import Title from ".";
+
+export default {
+  title: "common/Title",
+  component: Title,
+};
+const Template = () => <Title />;
+
+export const Default = Template.bind({});
---
+++ packages/stories/src/components/atom/Title/index.jsx
@@ -0,0 +1,8 @@
+// import { StyledThumbnail } from './Thumbnail.style';
+
+/* eslint-disable */
+const Title = () => {
+  return <h1>Hello</h1>;
+};
+
+export default Title;
