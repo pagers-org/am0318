5b7e6eb409c0f2183a5d478fa6fa274607fcc36a
areumsheep <areumsheep@gmail.com>
2022-03-06 03:55:07 +0900

    ✍️Design: 결과 페이지 디자인 수정

--- packages/frontend/src/components/ResultAward.jsx
+++ packages/frontend/src/components/ResultAward.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import Template from '../assets/images/template4.svg';
-import Stamp from '../assets/images/good_luck.svg';
+import Template from '../assets/images/award_blue.svg';
+import Stamp from '../assets/icons/stampImage1.svg';
 
 function ResultAward() {
   const today = new Date();
   const name = '노아';
-  const prize_name = '최고의밥상일이삼사오 ';
+  const prize_name = '최고의밥상 ';
   const detail = '다이어트 중에도 식용이 왕성해 이것저것 골고루 잘 먹는다';
   const sender = '반가워!';
   const date = `${today.getFullYear()}년 ${today.getMonth()}월 ${today.getDate()}일`;
@@ -35,7 +35,7 @@ function ResultAward() {
 export default ResultAward;
 
 const Wrapper = styled.main`
-  margin: 40px 0px;
+  margin: 30px 0px;
   width: 100%;
   display: flex;
   justify-content: center;
@@ -44,7 +44,7 @@ const Wrapper = styled.main`
 const AwardWrapper = styled.div`
   position: relative;
   border: 2px solid #000000;
-  max-width: 320px;
+  max-width: 330px;
   max-height: 410px;
   background-image: url(${Template});
   background-size: cover;
@@ -53,7 +53,7 @@ const AwardWrapper = styled.div`
   padding: 27px 40px;
 `;
 const AwardTitle = styled.h2`
-  font-size: 19px;
+  font-size: 18px;
   text-align: center;
   margin: 0px 0px 0px 7px;
   height: 45px;
