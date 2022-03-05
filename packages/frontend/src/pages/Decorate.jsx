6096e3c63056957e80e6b01f32d4c1255ba8703e
yechu <yechukim217@gmail.com>
2022-03-06 02:03:34 +0900

    ✒️Feat: 템플릿 추가

--- packages/frontend/src/pages/Decorate.jsx
+++ packages/frontend/src/pages/Decorate.jsx
@@ -4,7 +4,6 @@ import award_mint from '../assets/images/award_mint.svg';
 import award_blue from '../assets/images/award_blue.svg';
 import award_yellow from '../assets/images/award_yellow.svg';
 import award_orange from '../assets/images/award_orange.svg';
-import Loading from '../components/Loading';
 
 const awardsImage = [award_orange, award_yellow, award_mint, award_blue];
 
@@ -14,9 +13,11 @@ export const Decorate = () => {
   const [selectedAwardImg, setSelectedAwardImg] = useState(award_orange);
   return (
     <>
-
-      <Loading />
-
+      <Template
+        awardsImage={awardsImage}
+        selectedAwardImg={selectedAwardImg}
+        setSelectedAwardImg={setSelectedAwardImg}
+        setTemplateSelectDone={setTemplateSelectDone} />
     </>
   );
 };
