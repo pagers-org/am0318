a81a2b0ae32b39e451a2e03099a59aec9e463506
sunhwa <2017sunhwa@gmail.com>
2022-03-06 02:05:55 +0900

    ✒️Feat: 도장컴포넌트 개발 및 Decorate 페이지 구조 기본 설정

---
+++ packages/frontend/src/components/SelectStamp.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import Layout from './Layout';
+import styled from 'styled-components';
+import Button from '@project/stories/src/components/atom/Button';
+import { getImage, getStamp } from '../utils/GetSvg';
+const SelectStamp = ({ handleClick }) => {
+  const [selectedStamp, setSelectedStamp] = useState(0);
+  const DESC_STAMP = [
+    { title: 'GOOD LUCK', desc: '행운이 가득하다' },
+    { title: 'SUPER!!!', desc: '뭐든지 잘할 수 있다' },
+    { title: 'you are the best', desc: '세계 최고가 된다' },
+    { title: 'cheer up', desc: ' 힘이 솟는다' },
+    { title: 'love ya', desc: '사랑에 빠진다' },
+    { title: 'don’t worry', desc: '걱정이 마법처럼 사라진다' },
+  ];
+
+  const handleSelectStamp = index => {
+    setSelectedStamp(index);
+  };
+
+  return (
+    <Layout title={'도장을 찍고 완성하상!'}>
+      <StyledStamp selected={getImage(selectedStamp)}>
+        <div></div>
+      </StyledStamp>
+      <h4>{DESC_STAMP[selectedStamp]?.title}</h4>
+      <span>{DESC_STAMP[selectedStamp]?.desc}</span>
+      <StyledGrid>
+        {DESC_STAMP.map((item, index) => {
+          return (
+            <div key={item.title + item.desc} onClick={() => handleSelectStamp(index)}>
+              <img src={getStamp(index)} />
+            </div>
+          );
+        })}
+      </StyledGrid>
+      <Button theme="action" text="다음" onClick={handleClick} />
+    </Layout>
+  );
+};
+
+export default SelectStamp;
+
+const StyledGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 50px);
+  grid-template-rows: repeat(2, 50px);
+  grid-gap: 50px 30px;
+  margin-bottom: 20px;
+  div {
+    cursor: pointer;
+  }
+`;
+const StyledStamp = styled.div`
+  min-width: 216px;
+  min-height: 216px;
+  overflow: hidden;
+  div {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    background-image: ${({ selected }) => `url(${selected})`};
+  }
+`;
--- packages/frontend/src/pages/Decorate.jsx
+++ packages/frontend/src/pages/Decorate.jsx
@@ -1,6 +1,53 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import TextArea from '../components/TextArea';
+import Button from '@project/stories/src/components/atom/Button';
+import Layout from '../components/Layout';
+import Input from '../components/Input';
+import SelectStamp from '../components/SelectStamp';
 const Decorate = () => {
-  return <div>Decorate</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const navigate = useNavigate();
+
+  const handleNextButton = () => {
+    if (currentPage === 2) {
+      navigate('/Complete');
+    }
+    setCurrentPage(prev => prev + 1);
+  };
+
+  const handleInput = e => {
+    console.log(e.target.value);
+  };
+
+  const renderComponents = currentPage => {
+    switch (currentPage) {
+      case 0:
+        return (
+          <>
+            <h1>예츄 컴포넌트 자리</h1>
+          </>
+        );
+      case 1:
+        return (
+          <Layout title={'상장을 주고 싶은 이유를적어보상!'}>
+            <Input
+              placeholder={'ex. 친절한 어른상'}
+              name={'상장 이름(최대 10자)'}
+              handleInput={handleInput}
+              onChange={e => handleInput(e)}
+            />
+            <TextArea />
+            <Button theme="action" text="다음" onClick={() => handleNextButton()} />
+          </Layout>
+        );
+      case 2:
+        return <SelectStamp handleClick={() => handleNextButton()} />;
+      default:
+        break;
+    }
+  };
+
+  return <div>{renderComponents(currentPage)}</div>;
 };
 export default Decorate;
