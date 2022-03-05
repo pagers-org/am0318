5d3d3286e868697d2cd8ae8788cfcc00ecd71e66
sunhwa <2017sunhwa@gmail.com>
2022-03-05 14:59:23 +0900

    ✒️Feat: router 작업 및 페이지 파일 생성

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import Spinner from '@project/stories/src/components/atom/Spinner';
+import React, { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+const Home = lazy(() => import('./pages/Home'));
+const Main = lazy(() => import('./pages/Main'));
+const Decorate = lazy(() => import('./pages/Decorate'));
+const Complete = lazy(() => import('./pages/Complete'));
 
 function App() {
   return (
-    <div className="App">
-      <Spinner />
-    </div>
+    <Routes>
+      <Route path={'/'} element={<Home />} />
+      {/* 이름 등록 화면  */}
+      <Route path={'/main'} element={<Main />} />
+      {/* 상장 보내는 화면 */}
+      <Route path={'/main/:id'} element={<Main />} />
+      {/* 컬렉션 화면 */}
+      <Route path={'/main/:id/storage'} element={<Main />} />
+      <Route path={'/decorate'} element={<Decorate />} />
+      <Route path={'/complete'} element={<Complete />} />
+    </Routes>
   );
 }
 
