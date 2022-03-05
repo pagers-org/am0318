01ae004d78c30244a3374c3d99e8673d7802ff99
parksil0 <sirisiri0809@gmail.com>
2022-03-06 04:12:19 +0900

    ✒️Feat : api, service 초기 설정 및 회원가입 로직 추가

---
+++ packages/frontend/src/constants/index.js
@@ -0,0 +1,25 @@
+export const BASE_URL = 'https://am0318.herokuapp.com/api';
+// export const BASE_URL = 'http://localhost:4001/api'; // 로컬 서버
+
+export const ERROR_MESSAGE = {
+  HTTP_REQUEST_FAILURE: '요청에 실패했습니다.',
+};
+
+/**
+ style 관련 상수
+*/
+
+export const BUTTON_THEME = {
+  DEFAULT: 'default',
+  ACTION: 'action',
+  DISABLED: 'disabled',
+};
+
+/**
+ dto
+*/
+
+export const USER = {
+  NICKNAME: 'nickname',
+  USER_ID: 'userId',
+};
