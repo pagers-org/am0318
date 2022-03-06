ed2539c07916817c164d99fe71f678694ad34cd2
wooooooood <13pft13@gmail.com>
2022-03-06 17:32:37 +0900

    ✍️Design: 보관함 타이틀 컴포넌트 추가

---
+++ packages/stories/src/components/atom/StorageTitle/StorageTitle.stories.jsx
@@ -0,0 +1,9 @@
+import StorageTitle from ".";
+
+export default {
+  title: "common/StorageTitle",
+  component: StorageTitle,
+};
+const Template = () => <StorageTitle />;
+
+export const Default = Template.bind({});
---
+++ packages/stories/src/components/atom/StorageTitle/index.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import Template from "../../../assets/images/collection_background.svg";
+
+/* eslint-disable */
+const index = ({ children }) => {
+  return <StorageTitle>{children}</StorageTitle>;
+};
+
+export default index;
+
+export const StorageTitle = styled.h2`
+  background-image: url(${Template});
+  background-repeat: no-repeat;
+  text-align: center;
+  width: 305px;
+  height: 68px;
+  padding-top: 12px;
+`;
