bdca1898229ffafeda34b70c5da271c0cf298900
wooooooood <13pft13@gmail.com>
2022-03-06 21:30:14 +0900

    ✍️Design: 캐러셀 간격 media query 적용

--- packages/frontend/src/components/Carousel.jsx
+++ packages/frontend/src/components/Carousel.jsx
@@ -7,24 +7,47 @@ const Wrapper = styled.div`
   height: 100%;
   width: 100%;
 
-  .swiper {
-    width: 100%;
-    height: 100%;
-  }
   .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #fff;
-    display: flex;
+    display: inline-flex;
     justify-content: center;
-    align-items: center;
   }
 
-  .swiper-slide img {
-    display: block;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+  .swiper-slide.swiper-slide-prev{
+    margin-left: calc(100vw * 6% * -1);
+    padding-right: calc(100vw * 6%);
+  }
+  .swiper-slide.swiper-slide-next{
+    padding-left: calc(100vw * 6%);
+  }
+
+  @media only screen and (max-width: 425px) {
+    .swiper-slide.swiper-slide-prev{
+      margin-left: -130px;
+      padding-right: 130px;
+    }
+    .swiper-slide.swiper-slide-next{
+      padding-left: 130px;
+    }
+  }
+
+  @media only screen and (min-width: 426px) and (max-width: 550px) {
+    .swiper-slide.swiper-slide-prev{
+      margin-left: -90px;
+      padding-right: 90px;
+    }
+    .swiper-slide.swiper-slide-next{
+      padding-left: 90px;
+    }
+  }
+
+  @media only screen and (min-width: 551px) and (max-width: 700px) {
+    .swiper-slide.swiper-slide-prev{
+      margin-left: -60px;
+      padding-right: 60px;
+    }
+    .swiper-slide.swiper-slide-next{
+      padding-left: 60px;
+    }
   }
 `;
 
@@ -33,11 +56,11 @@ const Carousel = ({ children }) => {
     <Wrapper>
       <Swiper
         slidesPerView={2}
-        spaceBetween={30}
+        spaceBetween={20}
         centeredSlides={true}
         className="mySwiper"
       >
-        {children.map((c, key) => (
+        {(children || []).map((c, key) => (
           <SwiperSlide key={key}>{c}</SwiperSlide>
         ))}
       </Swiper>
