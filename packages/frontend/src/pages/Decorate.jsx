60263280e41d2680b1cd599a0966171ef718edd5
yechu <yechukim217@gmail.com>
2022-03-06 21:24:02 +0900

    🗃Refactor: 중복 코드 제거

--- packages/frontend/src/pages/Decorate.jsx
+++ packages/frontend/src/pages/Decorate.jsx
@@ -3,16 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import AwardContents from '../components/AwardContents';
 import SelectStamp from '../components/SelectStamp';
 import { Template } from '../components/Template';
-import award_mint from '../assets/images/award_mint.svg';
-import award_blue from '../assets/images/award_blue.svg';
-import award_yellow from '../assets/images/award_yellow.svg';
-import award_orange from '../assets/images/award_orange.svg';
 import Loading from '../components/Loading';
+import { getTemplate } from '../utils/GetSvg';
 
 const Decorate = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const navigate = useNavigate();
-  const TEMPLATE_IMAGE = [award_orange, award_yellow, award_mint, award_blue];
   const COLORS = ['#FF5F0F', '#FFF27B', '#82E8CF', '#79BBE5'];
   const [awardParams, setAwardParams] = useState({
     title: '',
@@ -46,7 +42,7 @@ const Decorate = () => {
             colors={COLORS}
             colorClicked={COLORS[Number(awardParams.templateId)]}
             changeItems={changeItems}
-            selectedAwardImg={TEMPLATE_IMAGE[Number(awardParams.templateId)]}
+            selectedAwardImg={getTemplate(Number(awardParams.templateId))}
             handleNextButton={() => handleNextButton()}
           />
         );
