c43fcdd7fd0abeca2d2c6b47290f4a05df21893f
parksil0 <sirisiri0809@gmail.com>
2022-03-07 00:19:23 +0900

    ✍️Design: layout title 여백 관련 재설정

--- packages/frontend/src/components/Layout.jsx
+++ packages/frontend/src/components/Layout.jsx
@@ -2,11 +2,23 @@ import { Title } from '@project/stories/src/components/atom/Title';
 import React from 'react';
 import styled from 'styled-components';
 
+/**
+ * @param {string[]} title: 문자열 배열로 받고, 배열 인덱스 단위는 줄넘김입니다.
+ * @param {ReactNode} children
+ */
+
 const Layout = ({ title, children }) => {
   return (
     <Wrapper>
       <header>
-        <LayoutTitle>{title}</LayoutTitle>
+        <LayoutTitle>
+          {title?.map((text, index) => (
+            <p key={index} style={{ margin: 0 }}>
+              {text}
+              {index !== title.length - 1 && <br />}
+            </p>
+          ))}
+        </LayoutTitle>
       </header>
       {children}
     </Wrapper>
@@ -25,6 +37,7 @@ const Wrapper = styled.main`
 
 const LayoutTitle = styled(Title)`
   font-size: 24px;
+  line-height: 41px;
   margin: 0px 36px;
   text-align: center;
 `;
