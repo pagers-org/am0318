5d8541e563c491017fb0cd7730926ac77a55793f
sunhwa <2017sunhwa@gmail.com>
2022-03-06 19:56:11 +0900

    ❗️✔️HOTFIX: api 연동 테스트중..

--- packages/frontend/src/service/index.js
+++ packages/frontend/src/service/index.js
@@ -1,15 +1,38 @@
-import { postCreateUser } from '../api';
+import { postCreateUser, postUpdateAward, getUser } from '../api';
 import { ERROR_MESSAGE, USER } from '../constants';
-import { setLocalStorage } from '../utils';
+import { setLocalStorage, getLocalStorage } from '../utils';
 
 const Service = () => {
   const self = {};
 
-  self.signup = async (nickname) => {
+  self.signup = async nickname => {
     try {
       const { data } = await postCreateUser(nickname);
       setLocalStorage(USER.USER_ID, data.data.userId);
       setLocalStorage(USER.NICKNAME, data.data.nickname);
+      return data.data;
+    } catch (e) {
+      console.log(e);
+      alert(ERROR_MESSAGE.HTTP_REQUEST_FAILURE);
+    }
+  };
+
+  self.getUser = async () => {
+    try {
+      const userId = getLocalStorage(USER.USER_ID);
+      const { data } = await getUser(userId);
+      return data.data;
+    } catch (e) {
+      console.log(e);
+      alert(ERROR_MESSAGE.HTTP_REQUEST_FAILURE);
+    }
+  };
+
+  self.updateAward = async awardParams => {
+    try {
+      const userId = getLocalStorage(USER.USER_ID);
+      const { data } = await postUpdateAward(userId, awardParams);
+      console.log('data', data);
       return data.data;
     } catch (e) {
       console.log(e);
