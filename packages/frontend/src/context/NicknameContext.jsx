229bd9040451715da54e35e4511d304328bd89c8
yechu <yechukim217@gmail.com>
2022-03-06 22:19:55 +0900

    Feat: 닉네임 context 추가

---
+++ packages/frontend/src/context/NicknameContext.jsx
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from 'react';
+
+const NicknameContext = createContext();
+
+export function NicknameContextProvider({ children }) {
+  const [yourNickname, setYourNickname] = useState('');
+  return (
+    <NicknameContext.Provider value={{ yourNickname, setYourNickname }}>
+      {children}
+    </NicknameContext.Provider>
+  );
+}
+
+export default NicknameContext;
