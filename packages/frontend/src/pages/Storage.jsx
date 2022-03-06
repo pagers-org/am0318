8bbcd8101efe63b53762beed5022449865784094
wooooooood <13pft13@gmail.com>
2022-03-07 00:08:27 +0900

    ✒️Feat: 상장이 없는 경우

--- packages/frontend/src/pages/Storage.jsx
+++ packages/frontend/src/pages/Storage.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import styled from "styled-components";
 import StorageTitle from "@project/stories/src/components/atom/StorageTitle";
 import Button from "@project/stories/src/components/atom/Button";
-import blue from "../assets/images/award_blue.svg";
 import { BUTTON_THEME, SEND_URL, TIMEOUT, USER } from "../constants";
 import { useNavigate } from "react-router-dom";
-import ResultAward from '../components/ResultAward';
+import ResultAward from "../components/ResultAward";
+import CloseIcon from "../assets/icons/close.svg";
+import EmptyImage from "../assets/images/award-empty.svg";
 
 import Service from "../service";
 import { getLocalStorage } from "../utils";
@@ -16,26 +17,73 @@ const localstorageNickname = getLocalStorage(USER.NICKNAME);
 
 const Storage = () => {
   const [nickname, setNickname] = useState(localstorageNickname || "");
+  const [selectedAward, setSelectedAward] = useState(null);
+  const [awardParamList, setAwardParamList] = useState([
+    { templateId: 0, stickerId: 1 },
+  ]);
 
   const navigate = useNavigate();
   const service = Service();
+
+  useEffect(() => {
+    //setAwardParamList(service.getAwards());
+  }, []);
+
+  const showAwards = () => {
+    if (awardParamList?.length > 0) {
+      return (
+        <CarouselWrapper>
+          <Carousel>
+            {awardParamList.map((x, key) => (
+              <AwardWrapper key={key} onClick={onAwardClick}>
+                <ResultAward awardParam={x} />
+              </AwardWrapper>
+            ))}
+          </Carousel>
+        </CarouselWrapper>
+      );
+    } else {
+      console.log("Empty");
+      return (
+        <EmptyWrapper>
+          <img src={EmptyImage} />
+        </EmptyWrapper>
+      );
+    }
+  };
+
+  const onAwardClick = () => {
+    // setSelectedAward(1);
+  };
+
+  const onBackbuttonClick = () => {
+    if (selectedAward) {
+      setSelectedAward(null);
+    } else {
+      navigate(-1);
+    }
+  };
 
   return (
     <Layout title="상장을 보며 자신감을 채우상!">
       <Wrapper>
         <StorageTitle>{nickname}의 컬렉션</StorageTitle>
-        <CarouselWrapper>
-          <Carousel>
-            <AwardWrapper><ResultAward awardParam={{stickerId: 0}} /></AwardWrapper>
-            <AwardWrapper><ResultAward awardParam={{stickerId: 1}} /></AwardWrapper>
-            <AwardWrapper><ResultAward awardParam={{stickerId: 2}} /></AwardWrapper>
-            <AwardWrapper><ResultAward awardParam={{stickerId: 3}} /></AwardWrapper>
-          </Carousel>
-        </CarouselWrapper>
+        {selectedAward ? (
+          <DetailWrapper>
+            <CloseButton onClick={onBackbuttonClick}>
+              <img src={CloseIcon} />
+            </CloseButton>
+
+            <ResultAward awardParam={{ stickerId: 0 }} />
+          </DetailWrapper>
+        ) : (
+          showAwards()
+        )}
+
         <Button
           theme={BUTTON_THEME.DEFAULT}
           text="뒤로 가기"
-          onClick={() => navigate(-1)}
+          onClick={onBackbuttonClick}
         />
       </Wrapper>
     </Layout>
@@ -60,16 +108,25 @@ const CarouselWrapper = styled.div`
   }
 `;
 
+const EmptyWrapper = styled.div`
+  max-width: 768px;
+  height: 410px;
+  margin-bottom: 120px;
+  flex-direction: column;
+  display: flex;
+  justify-content: end;
+
+  img {
+    height: 95%;
+  }
+`;
+
 const AwardWrapper = styled.div`
   max-width: 330px;
-`
+`;
 
-const ButtonWrapper = styled.div`
-  position: fixed;
-  bottom: 70px;
+const CloseButton = styled.button``;
+
+const DetailWrapper = styled.div`
   display: flex;
-  gap: 20px;
-  flex-direction: column;
-  justify-content: flex-end;
-  margin: 100px 0;
 `;
